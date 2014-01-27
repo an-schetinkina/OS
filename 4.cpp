7466e3cfac50096a0539a7ea32b581183f57347f
Nastya <nastya.schetinkina@yandex.ru>
2014-01-28 00:30:17 +0600

    Correct 9 task

--- 4.cpp
+++ 4.cpp
@@ -8,39 +8,34 @@
 // блоков 16;
 // 2048/16 = 128;
 
-
-size_t** matrix1, ** matrix2, m_result[2048][2048];
-size_t**** block_matrix1,**** block_matrix2, b_result[128][16][128][16];
+size_t matrix1[2048][2048], matrix2[2048][2048], m_result[2048][2048];
+size_t block_matrix1[128][128][16][16], block_matrix2[128][128][16][16], b_result[128][128][16][16];
 
 void* matrix_mul(void *id) {
     size_t* thread = (size_t*)(id);
-    for (size_t i=0; i<2048; i++) {
-        if (i%7 == *thread) {
-            for (size_t j=0; j<2048; j++) {
-                size_t res = 0;
-                for (size_t k=0; k<2048; k++) {
-                    res += matrix1[i][k] * matrix2[k][j];
-                }
-                m_result[i][j] = res;
+
+    for (size_t i=*thread; i<2048; i+=7) {
+        for (size_t j=0; j<2048; j++) {
+			size_t res = 0;
+            for (size_t k = 0; k < 2048; k++) {
+                res += matrix1[i][k] * matrix2[k][j];
             }
+			m_result[i][j] = res;
         }
     }
 }
 
-void* block_matrix_mul(void *id)
-{
+void* block_matrix_mul(void *id) {
+
     size_t* thread = (size_t*)(id);
-    for (size_t i=0; i<2048; i++) {
-        if (i%7 == *thread) {
-            for (size_t j=0; j<2048; j++) {
-                size_t res = 0;
-                for (size_t k=0; k<2048; k++) {
-                    res += block_matrix1[i/16][i%16][k/16][k%16] * block_matrix2[k/16][k%16][j/16][j%16];
-                }
-                b_result[i/16][i%16][j/16][j%16] = res;
-            }
-        }
-    }
+
+    for (size_t i=*thread; i<2048; i+=7) 
+        for (size_t j=0; j<128; j++)
+            for (size_t jj=0; jj<16; jj++)
+                for (size_t k=0; k<128; k++)
+                    for (size_t kk=0; kk<16; kk++)
+                        b_result[i/16][j][i%16][jj] += block_matrix1[i/16][k][i%16][kk] * block_matrix2[k][j][kk][jj];
+
 }
 
 
@@ -67,30 +62,6 @@ void testing_block_matrix()
 
 int main(void)
 {
-	//matrix initialization
-    matrix1 = new size_t*[2048];
-    matrix2 = new size_t*[2048];
-    block_matrix1 = new size_t***[128];
-    block_matrix2 = new size_t***[128];
-
-    for (size_t i=0; i<2048; i++) {
-        matrix1[i] = new size_t[2048];
-        matrix2[i] = new size_t[2048];
-    }
-
-    for (size_t i=0; i<128; i++) {
-        block_matrix1[i] = new size_t**[16];
-        block_matrix2[i] = new size_t**[16];
-        for (size_t j=0; j<16; j++) {
-            block_matrix1[i][j] = new size_t*[128];
-            block_matrix2[i][j] = new size_t*[128];
-            for (size_t k=0; k<128; k++) {
-                block_matrix1[i][j][k] = new size_t[16];
-                block_matrix2[i][j][k] = new size_t[16];
-            }
-        }
-    }
-
 	float start, end;
 
 	//test matrix
